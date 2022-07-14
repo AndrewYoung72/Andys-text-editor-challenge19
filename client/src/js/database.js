a6950bd036d6c71148c0743049fea0c61b930b44
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-07-14 09:50:02 -0600

    postdb added to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+export const postDb = async (content) => {
+  console.error("postDb not implemented");
+  const editorDb = await openDB("edits", 1);
+  const tx = editorDb.transaction("edits", "readwrite");
+  const store = tx.objectStore("edits");
+  const request = store.add({ edit: content });
+  const result = await request;
+  console.log("data saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
