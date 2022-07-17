553db3ebf64bd5b86525d070b868319345965198
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-07-17 14:29:17 -0600

    client work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,19 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   try {
     const editorDb = await openDB("jate", 1);
     const tx = editorDb.transaction("jate", "readwrite");
     const store = tx.objectStore("jate");
-    const request = store.add({ id: id, edit: content });
+    const request = store.add({ edit: content });
     const result = await request;
     console.log("data saved to the database", result);
   } catch (err) {
     console.error(err);
   }
 };
-
+//PostDb
+// export const postDb = async (content) => {
+//   try {
+//   const editorDb = await openDB("jate", 1);
+//   const tx = editorDb.transaction("jate", "readwrite");
+//   const store = tx.objectStore("jate");
+//   const request = store.add({ edit: content });
+//   const result = await request;
+//   console.log("data saved to the database", result);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   try {
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,45 +1,45 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin',
+        template: "./index.html",
+        title: "Webpack Plugin",
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
         // filename: "manifest.json",
-        name: 'JATE',
+        name: "JATE",
         start_url: "./",
         publicPath: "./",
-        short_name: 'manifest',
-        description: 'just another text editor!',
+        short_name: "manifest",
+        description: "just another text editor!",
         icons: [
           {
-            src: path.resolve('./src/images/logo.png'),
+            src: path.resolve("./src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('images', 'logo.png'),
+            destination: path.join("images", "icons"),
           },
         ],
       }),
@@ -49,16 +49,19 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
