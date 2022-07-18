4db566d1277c636da57e58b94984b5049d12425d
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-07-18 08:14:54 -0600

    fine tuning, looking better, still getting codemirror error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,12 +15,12 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   try {
-    const editorDb = await openDB("jate", 1);
-    const tx = editorDb.transaction("jate", "readwrite");
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readwrite");
     const store = tx.objectStore("jate");
-    const request = store.add({ edit: content });
+    const request = store.put({ value: content });
     const result = await request;
-    console.log("data saved to the database", result);
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
@@ -41,8 +41,8 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   try {
-    const editorDb = await openDB("jate", 1);
-    const tx = editorDb.transaction("jate", "readonly");
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readonly");
     const store = tx.objectStore("jate");
     const request = store.getAll();
     const result = await request;
